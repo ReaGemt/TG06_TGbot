eac0b68787ab97acd94dca73ce964243c5e9033b
ReaGemt <zero2028@inbox.ru>
2024-09-26 10:59:00 +0300

    add script

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import asyncio
 import random
 import sqlite3
-import aiohttp
 import logging
 import requests
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
@@ -267,4 +266,4 @@ async def main():
    await dp.start_polling(bot)
 
 if __name__ == '__main__':
-   asyncio.run(main())
+    asyncio.run(main())
